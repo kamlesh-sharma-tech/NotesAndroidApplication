3d1e39d14b8c9c00490ea6f2ba6c55147cc9d91e
Kamlesh Sharma <66367321+kamlesh-sharma-tech@users.noreply.github.com>
2022-07-01 12:32:53 +0530

    Update UpdateActivity.java
    made some changes

--- app/src/main/java/com/android/sqlitedatabase/UpdateActivity.java
+++ app/src/main/java/com/android/sqlitedatabase/UpdateActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +16,8 @@ import android.widget.TextView;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import io.github.muddz.styleabletoast.StyleableToast;
 
 public class UpdateActivity extends AppCompatActivity {
     TextView updateNoteId;
@@ -59,26 +62,47 @@ public class UpdateActivity extends AppCompatActivity {
         updateNote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                updateNotes();
+                updateNote();
             }
         });
 
     }
 
 
-    public void updateNotes() {
-        DataBase db = new DataBase(UpdateActivity.this);
-        db.updateNote(title_id.getText().toString(),desc_id.getText().toString(),id);
-
-        Intent intent = new Intent(UpdateActivity.this,MainActivity.class);
-        intent.putExtra("id",id);
-        intent.putExtra("title",title_id.getText().toString());
-        intent.putExtra("desc",desc_id.getText().toString());
-        intent.putExtra("date",currentDate);
-        intent.putExtra("time",currentTime);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
-        finish();
+    public void updateNote() {
+        if(title_id.getText().toString().isEmpty()){
+            new StyleableToast
+                    .Builder(UpdateActivity.this)
+                    .text("Note Title is required")
+                    .textColor(Color.BLACK)
+                    .backgroundColor(Color.WHITE)
+                    .textSize(18)
+                    .cornerRadius(8)
+                    .length(25)
+                    .show();
+        }else if(desc_id.getText().toString().isEmpty()){
+            new StyleableToast
+                    .Builder(UpdateActivity.this)
+                    .text("Message is required")
+                    .textColor(Color.BLACK)
+                    .backgroundColor(Color.WHITE)
+                    .textSize(18)
+                    .cornerRadius(8)
+                    .length(25)
+                    .show();
+        }else {
+            DataBase db = new DataBase(UpdateActivity.this);
+            db.updateNote(title_id.getText().toString(), desc_id.getText().toString(), id);
+            Intent intent = new Intent(UpdateActivity.this, MainActivity.class);
+            intent.putExtra("id", id);
+            intent.putExtra("title", title_id.getText().toString());
+            intent.putExtra("desc", desc_id.getText().toString());
+            intent.putExtra("date", currentDate);
+            intent.putExtra("time", currentTime);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            finish();
+        }
 
     }
 }
